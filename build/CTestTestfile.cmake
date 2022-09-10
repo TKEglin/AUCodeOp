d6c04fb132894a5a5babfc3066444f1fd7c9731d
Muhsinscott112 <91264802+Muhsinscott112@users.noreply.github.com>
2022-09-10 15:25:04 +0200

    .

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/madaxweyne/Documents/Computer Science/3rd Semester/Software Technology/AUCodeOp-1/src
-# Build directory: /Users/madaxweyne/Documents/Computer Science/3rd Semester/Software Technology/AUCodeOp-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
